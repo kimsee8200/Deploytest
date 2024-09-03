f36dc7b437836f31eb7d4ca28c755dd6eea1ec87
kimsee8200 <ttungseui11@gmail.com>
2024-09-03 12:25:50 +0900

    Initial commit

---
+++ src/test/java/org/example/projectfordeploy/ProjectForDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projectfordeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectForDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
